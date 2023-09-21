3985eac58f341a6b02884b079a4952ec0fb64b28
tolluset <dlqud19@gmail.com>
2023-09-22 01:03:12 +0900

    feat: make og image

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  webpack: (config) => {
+    config.externalsType = "commonjs";
+    config.externals = config.externals || [];
+    config.externals.push({
+      "@napi-rs/image": "@napi-rs/image",
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import { Metadata, ResolvingMetadata } from "next";
 import path from "path";
-import fs from "fs";
+import fs, { promises } from "fs";
 import satori from "satori";
-import Head from "next/head";
-
-const config = {};
+import { Transformer } from "@napi-rs/image";
 
 export const dynamic = "force-static";
 
@@ -12,9 +10,9 @@ export async function generateMetadata(
   {},
   parent: ResolvingMetadata
 ): Promise<Metadata> {
-  const fontPath = path.resolve("./src/fonts/Mooli-Regular.ttf");
-  const m = fs.readFileSync(fontPath);
-  const ogImg = await satori(
+  const fontPath = path.resolve("./src/fonts/NotoSansKR-Regular.ttf");
+  const font = fs.readFileSync(fontPath);
+  const svg = await satori(
     <div
       style={{
         height: "100%",
@@ -35,8 +33,8 @@ export async function generateMetadata(
       height: 400,
       fonts: [
         {
-          name: "Mooli",
-          data: m,
+          name: "NotoSansKR-Regular",
+          data: font,
           weight: 400,
           style: "normal",
         },
@@ -44,11 +42,16 @@ export async function generateMetadata(
     }
   );
 
+  const transformer = Transformer.fromSvg(svg);
+  const png = await transformer.png();
+
+  await promises.writeFile("./public/og.png", png);
+
   return {
     title: "OG Satori Example",
     description: "This is an example of using Satori to generate OG images 🐰",
     openGraph: {
-      images: ogImg,
+      images: "./public/og.png",
     },
   };
 }
@@ -57,64 +60,6 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       check out the og image 🐰
-      <OG />
     </main>
-  );
-}
-
-function OG() {
-  return (
-    <div
-      style={{
-        display: "flex",
-        height: "100%",
-        width: "100%",
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-        backgroundImage: "linear-gradient(to bottom, #dbf4ff, #fff1f1)",
-        fontSize: 60,
-        letterSpacing: -2,
-        fontWeight: 700,
-        textAlign: "center",
-      }}
-    >
-      <div
-        style={{
-          backgroundImage:
-            "linear-gradient(90deg, rgb(0, 124, 240), rgb(0, 223, 216))",
-          backgroundClip: "text",
-          // @ts-ignore
-          "-webkit-background-clip": "text",
-          color: "transparent",
-        }}
-      >
-        Make OG Image
-      </div>
-      <div
-        style={{
-          backgroundImage:
-            "linear-gradient(90deg, rgb(121, 40, 202), rgb(255, 0, 128))",
-          backgroundClip: "text",
-          // @ts-ignore
-          "-webkit-background-clip": "text",
-          color: "transparent",
-        }}
-      >
-        In
-      </div>
-      <div
-        style={{
-          backgroundImage:
-            "linear-gradient(90deg, rgb(255, 77, 77), rgb(249, 203, 40))",
-          backgroundClip: "text",
-          // @ts-ignore
-          "-webkit-background-clip": "text",
-          color: "transparent",
-        }}
-      >
-        Build time
-      </div>
-    </div>
   );
 }
