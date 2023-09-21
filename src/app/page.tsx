8fb479a1b730435dc5a58d6f6e35d485b7d8a790
tolluset <dlqud19@gmail.com>
2023-09-22 01:10:22 +0900

    chore: use metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,60 +6,69 @@ import { Transformer } from "@napi-rs/image";
 
 export const dynamic = "force-static";
 
-export async function generateMetadata(
-  {},
-  parent: ResolvingMetadata
-): Promise<Metadata> {
-  const fontPath = path.resolve("./src/fonts/NotoSansKR-Regular.ttf");
-  const font = fs.readFileSync(fontPath);
-  const svg = await satori(
-    <div
-      style={{
-        height: "100%",
-        width: "100%",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#fff",
-        fontSize: 32,
-        fontWeight: 600,
-      }}
-    >
-      <div style={{ marginTop: 40 }}>빌드에들어가나 🐰</div>
-    </div>,
-    {
-      width: 600,
-      height: 400,
-      fonts: [
-        {
-          name: "NotoSansKR-Regular",
-          data: font,
-          weight: 400,
-          style: "normal",
-        },
-      ],
-    }
-  );
+// export async function generateMetadata(
+//   {},
+//   parent: ResolvingMetadata
+// ): Promise<Metadata> {
+//   const fontPath = path.resolve("./src/fonts/NotoSansKR-Regular.ttf");
+//   const font = fs.readFileSync(fontPath);
+//   const svg = await satori(
+//     <div
+//       style={{
+//         height: "100%",
+//         width: "100%",
+//         display: "flex",
+//         flexDirection: "column",
+//         alignItems: "center",
+//         justifyContent: "center",
+//         backgroundColor: "#fff",
+//         fontSize: 32,
+//         fontWeight: 600,
+//       }}
+//     >
+//       <div style={{ marginTop: 40 }}>빌드에들어가나 🐰</div>
+//     </div>,
+//     {
+//       width: 600,
+//       height: 400,
+//       fonts: [
+//         {
+//           name: "NotoSansKR-Regular",
+//           data: font,
+//           weight: 400,
+//           style: "normal",
+//         },
+//       ],
+//     }
+//   );
 
-  const transformer = Transformer.fromSvg(svg);
-  const png = await transformer.png();
+//   const transformer = Transformer.fromSvg(svg);
+//   const png = await transformer.png();
 
-  await promises.writeFile("./public/og.png", png);
+//   await promises.writeFile("./public/og.png", png);
 
-  return {
-    title: "OG Satori Example",
-    description: "This is an example of using Satori to generate OG images 🐰",
-    openGraph: {
-      images: "./public/og.png",
-    },
-  };
-}
+//   return {
+//     title: "OG Satori Example",
+//     description: "This is an example of using Satori to generate OG images 🐰",
+//     openGraph: {
+//       images: "./og.png",
+//     },
+//   };
+// }
+
+export const metadata: Metadata = {
+  title: "OG Satori Example",
+  description: "This is an example of using Satori to generate OG images 🐰",
+  openGraph: {
+    images: "./og.png",
+  },
+};
 
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       check out the og image 🐰
+      <img src="./og.png" />
     </main>
   );
 }
